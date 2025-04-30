9042be69533de11c8a21af7582d9c24546c0f1c6
octavio <vizdoagaoctavio33@gmail.com>
2025-04-30 22:38:33 +0300

    Add key event listener to check letter input

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const text = document.querySelector('#text').innerHTML;
 let letters = [];
+let letterElements = [];
+
+let currentLetter = 0;
 
 for(let i = 0; i < text.length; i++){
   letters.push(text[i]);
@@ -10,4 +13,17 @@ letters.forEach(letter => {
   newLetter.innerHTML = letter;
   newLetter.className = 'letter';
   document.body.appendChild(newLetter);
+  letterElements.push(newLetter);
 });
+
+function getKey(event){
+  if(event.key === letters[currentLetter]){
+    currentLetter++;
+    console.log('good');
+  } else {
+    currentLetter++;
+    console.log('bad');
+  }
+}
+
+document.body.addEventListener('keydown', getKey);
