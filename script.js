15c548ee7f08fde8ea86337b4c39f6caf479dad3
octavio <vizdoagaoctavio33@gmail.com>
2025-04-30 21:52:52 +0300

    Split text into individual letters and display them inline

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const text = document.querySelector('#text');
+const text = document.querySelector('#text').innerHTML;
+let letters = [];
+
+for(let i = 0; i < text.length; i++){
+  letters.push(text[i]);
+}
+
+letters.forEach(letter => {
+  let newLetter = document.createElement('p');
+  newLetter.innerHTML = letter;
+  newLetter.className = 'letter';
+  newLetter.style.display = 'inline';
+  document.body.appendChild(newLetter);
+});
