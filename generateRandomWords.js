3183673ab2d162545047f3d2c15b1b7a6169b34a
octavio <vizdoagaoctavio33@gmail.com>
2025-05-01 11:41:44 +0300

    Generate 2000 random words and add them to the .json file generateRandomWords.js generates 2000 random words and adds them to the randomWords.json file

---
+++ generateRandomWords.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const randomWords = require('random-words');
+const FILE = 'randomWords.json';
+const TOTAL_WORDS = 2000;
+const stream = fs.createWriteStream(FILE);
+
+stream.write('[');
+
+for (let i = 0; i < TOTAL_WORDS; i++) {
+  const word = JSON.stringify(randomWords());
+  stream.write(word + (i < TOTAL_WORDS - 1 ? ',' : ''));
+}
+
+stream.write(']');
+stream.end();
